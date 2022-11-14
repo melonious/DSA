132a7c9324608f692663f91c426c4b9ff3489158
Deanna Melin <deannamelin@gmail.com>
2022-11-14 08:04:22 -0800

    finish quick sort implementation

--- reference/sorting/quick_sort.rb
+++ reference/sorting/quick_sort.rb
@@ -4,24 +4,32 @@
 # space complexity: O(n)
 
 def quick_sort(arr, left=0, right=(arr.length - 1))
-    
+    if left < right
+        pivot_idx = pivot_helper(arr, left, right)
+        # left
+        quick_sort(arr, left, (pivot_idx - 1))
+        #right
+        quick_sort(arr, (pivot_idx + 1), right)
+    end
+    return arr
 end
 
-def pivot_helper(arr, start=0, end=(arr.length - 1))
+def pivot_helper(arr, left=0, right=(arr.length - 1))
     # assumes pivot is always first element in arr
-    pivot = arr[start]
-    swap_idx = start
+    pivot = arr[left]
+    swap_idx = left
 
-    i = start + 1
-    while i <= end
+    i = left + 1
+    while i <= right
         if pivot > arr[i]
             swap_idx += 1
             swap(arr, swap_idx, i)
         end
+        i += 1
     end
 
     # swap the pivot from the start to the new pivot point
-    swap(arr, start, swap_idx)
+    swap(arr, left, swap_idx)
     return swap_idx
 end
 
@@ -30,11 +38,11 @@ def swap(arr, idx1, idx2)
 end
 
 # test cases
-puts merge_sort([2,1,9,76,4])
+puts quick_sort([2,1,9,76,4])
 puts "==========="
 
-puts merge_sort([134,48,12,90,-5])
+puts quick_sort([134,48,12,90,-5])
 puts "==========="
 
-puts merge_sort([17,0,3,17,-82,17])
+puts quick_sort([17,0,3,17,-82,17])
 puts "==========="
